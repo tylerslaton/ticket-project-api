525850907b8cf63c4df9045d75988e9862abf2ed
dbdoyle182 <dbdoyle182@gmail.com>
2020-10-25 19:49:03 -0400

    wrapped up event routes

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -11,36 +11,29 @@ const createEvent = async (event) => {
   }
 }
 
-const updateEvent = async (event, id) => {
-  Event.update(event, { where : { id }})
-    .then(result => {
-      console.log(result);
-      return result;
-    })
-    .catch(err => {
-      console.log(err);
-      return err;
-    })
+const updateEvent = async (event) => {
+  try {
+    const updatedEvent = await Event.upsert(event, { returning: true });
+    return updatedEvent;
+  } catch(err) {
+    return err;
+  }
 }
 
 const findEvent = async (id) => {
   try {
     const event = await Event.findOne({ where: { id }})
-    console.log(event);
     return event;
   } catch(err) {
-    console.log(err);
+    return err;
   }
 }
 
 const findEvents = async (where) => {
   try {
     const events = await Event.findAll({ where });
-
-    console.log(events);
+    return events;
   } catch (err) {
-    console.log(err);
-
     return err;
   }
 }
