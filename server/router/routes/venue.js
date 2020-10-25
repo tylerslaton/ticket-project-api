7d6d2848638b919607201603b10f331737121924
tylerslaton <mslaton@redventures.com>
2020-10-25 11:39:04 -0400

    Add delete functionality for venues

--- server/router/routes/venue.js
+++ server/router/routes/venue.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 const { Venue } = require('../../models');
 
-// Register Route
+// Create Route
 router.post('/', async (req, res) => {
     try {
         // Create the venue with the supplied data
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
         }
 
         // Respond with a 404 otherwise
-        return res.status(404)
+        return res.status(404).send()
 
     } catch(err) {
         // If an error occurs, respond with a 400 error. Always blame it on the user B)
@@ -46,18 +46,38 @@ router.get('/:id', async (req, res) => {
 // Update route
 router.put('/:id', async (req, res) => {
     try {
-        // Create the venue with the supplied data
-        let id = req.params.id
-        let venue = await Venue.findOne({ where: { id } });
+        // If the venue is found and there is a body, update it with the new data
+        if (req.body != {}) {
+            let id = req.params.id
+            let updated = await Venue.update(req.body, { where : { id } });
 
-        // If the venue is found, respond update it with the new data
-        if (venue) {
-            let resp = await venue.update(req.body);
-            return res.status(201).json(resp);
+            // Determine if the update went through
+            let status = updated[0] > 0 ? 200 : 404
+
+            return res.status(status).send();
         }
 
-        // Respond with a 404 otherwise
-        return res.status(404)
+        // Respond with a 400 otherwise
+        return res.status(400).send('did not send a body with the request')
+
+    } catch(err) {
+        console.log(err);
+        // If an error occurs, respond with a 400 error. Always blame it on the user B)
+        return res.status(400).send(err);
+    }    
+});
+
+// Delete route
+router.delete('/:id', async (req, res) => {
+    try {
+        // Create the venue with the supplied data
+        let id = req.params.id
+        let deleted = await Venue.destroy({where: {id}})
+    
+        // Determine if the delete went through
+        let status = deleted ? 200 : 404
+
+        return res.status(status).send();
 
     } catch(err) {
         // If an error occurs, respond with a 400 error. Always blame it on the user B)
